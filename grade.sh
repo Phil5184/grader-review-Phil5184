9ef3c2cceadc37626c2ecd97aa109afbfadf2edb
Phil5184 <p3mai@ucsd.edu>
2023-11-07 12:54:03 -0800

    changes

--- grade.sh
+++ grade.sh
@@ -1,3 +1,4 @@
+set -e
 CPATH='.:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar'
 
 rm -rf student-submission
@@ -8,7 +9,17 @@ mkdir grading-area
 git clone $1 student-submission
 echo 'Finished cloning'
 
-
+Files=`find -name "*.java"`
+expected="ListExamples.java"
+for file in $Files
+do
+    if [[ -f $file ]] && [[ $file == $expected ]]
+    then 
+        echo "Correct file found"
+        exit
+    fi 
+done
+cp student-submission/ListExamples.java grading-area
 # Draw a picture/take notes on the directory structure that's set up after
 # getting to this point
 
