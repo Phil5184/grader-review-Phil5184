1abc21fabc13894cfd9a67a3366cbf37b68d31d3
Phil5184 <p3mai@ucsd.edu>
2023-11-28 12:41:03 -0800

    grade.sh wont work

--- grade.sh
+++ grade.sh
@@ -1,4 +1,4 @@
-set -e
+//set -e
 CPATH='.:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar'
 
 rm -rf student-submission
@@ -9,17 +9,40 @@ mkdir grading-area
 git clone $1 student-submission
 echo 'Finished cloning'
 
-Files=`find -name "*.java"`
+Files=`find . -name "*.java"`
 expected="ListExamples.java"
 for file in $Files
 do
-    if [[ -f $file ]] && [[ $file == $expected ]]
+    if [[ -f $file ]] && [[ $file == **/ListExamples.java* ]]
     then 
         echo "Correct file found"
         exit
     fi 
 done
 cp student-submission/ListExamples.java grading-area
+
+cp -r lib grading-area
+
+cd grading-area
+
+javac -cp $CPATH *.java
+
+if [[ $? != 0 ]]
+then 
+    echo compilation error
+fi
+java -cp $CPATH org.junit.runner.JUnitCore TestListExamples > test.txt
+
+grep -oE run: [0-9]+ grading-area/test.txt > Tests.txt
+numTests = grep -oE [0-9]+ Tests.txt
+
+grep -oE Failures: [0-9]+ grading-area/test.txt > Failures.txt
+numFail = grep -oE [0-9]+ Failures.txt
+
+echo $numFail
+$grade = $numFail/$numTests
+echo $grade
+grep "test" test.txt
 # Draw a picture/take notes on the directory structure that's set up after
 # getting to this point
 
